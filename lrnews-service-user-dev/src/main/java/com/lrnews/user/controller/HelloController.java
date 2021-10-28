e5dada60a75b08f1621cb0b387dc7f5d4874762b
lawrence <751074877@qq.com>
2021-10-28 19:16:12 +0800

    Add logback configuration

--- lrnews-service-user-dev/src/main/java/com/lrnews/user/controller/HelloController.java
+++ lrnews-service-user-dev/src/main/java/com/lrnews/user/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.lrnews.user.controller;
 
 import com.lrnews.api.controller.user.HelloControllerApi;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController implements HelloControllerApi {
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     public Object hello(){
         return "hello";
