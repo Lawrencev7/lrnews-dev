108ba3ec2cb0c0bc083aa5622a634972abc9e58c
lawrence <751074877@qq.com>
2022-01-06 11:11:08 +0800

    Not-Version commit
    Add mybatis generated files.

---
+++ mybatis-generator-database/src/main/java/com/lrnews/admin/mapper/AdminUserMapper.java
@@ -0,0 +1,7 @@
+package com.lrnews.admin.mapper;
+
+import com.lrnews.my.mapper.MyMapper;
+import com.lrnews.pojo.AdminUser;
+
+public interface AdminUserMapper extends MyMapper<AdminUser> {
+}
---
+++ mybatis-generator-database/src/main/java/com/lrnews/user/mapper/AppUserMapper.java
@@ -0,0 +1,7 @@
+package com.lrnews.user.mapper;
+
+import com.lrnews.my.mapper.MyMapper;
+import com.lrnews.pojo.AppUser;
+
+public interface AppUserMapper extends MyMapper<AppUser> {
+}
